60ad903b1db8bcab17b612d50dc153595f54a5a7
Ayush H <jqghebri@gmail.com>
2024-09-24 16:35:08 +0530

    creation

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,30 @@
+import memojiImage from "@/assets/images/memoji-computer.png"
+import Image from "next/image"
+import ArrowDown from "@/assets/icons/arrow-down.svg"
+
 export const HeroSection = () => {
-  return <div>Hero Section</div>;
-};
+  return (
+  <div className="py-32 ">
+    <div className="container">
+      <Image src={memojiImage} alt="Person peaking from behind laptop" />
+      <div>
+        <div>
+
+        </div>
+        <div>Available for new projects</div>
+      </div>
+      <h1>building Esceptional User Experiences</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis ipsa atque quod commodi. Provident voluptates perferendis est vel sit totam, eveniet dolorem aliquam ullam in itaque ratione odio, quaerat officiis.</p>
+      <div>
+        <button>
+          <span>Explore My Work!</span>
+          <ArrowDown />
+        </button>
+        <button>
+          <span>My Resume</span>
+        </button>
+      </div>
+    </div>
+  </div>
+  )
+}
