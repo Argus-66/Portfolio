5ff5bcd7989fd2dedca88f973b1bef6f9cb3aa5b
Ayush H <jqghebri@gmail.com>
2024-09-24 16:06:28 +0530

    creation

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,8 +1,8 @@
 export const Header = () => {
   return(
-    <div className="flex justify-center items-center">
+    <div className="flex justify-center items-center relative top-3">
       <nav>
-        <a href="#" >Home</a>
+        <a href="#" className="nav-item">Home</a>
         <a href="">Projects</a>
         <a href="">About</a>
         <a href="">Contact</a>
