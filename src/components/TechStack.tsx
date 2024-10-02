5270ecb941e0e5d830d2e833a7d32fec14bcbe9a
Ayush H <jqghebri@gmail.com>
2024-10-02 23:59:20 +0530

    creation

---
+++ src/components/TechStack.tsx
@@ -0,0 +1,33 @@
+import JavascriptIcon from "@/assets/icons/square-js.svg";
+import HTMLIcon from "@/assets/icons/html5.svg";
+import CssIcon from "@/assets/icons/css3.svg";
+import Reacticon from "@/assets/icons/react.svg";
+import ChromeIcon from "@/assets/icons/chrome.svg";
+import GithubIcon from "@/assets/icons/github.svg";
+
+export const toolboxItems = [
+    {
+        title: "JavaScript",
+        icon: <JavascriptIcon />,
+    },
+    {
+        title: "HTML5",
+        icon: <HTMLIcon />,
+    },
+    {
+        title: "CSS3",
+        icon: <CssIcon />,
+    },
+    {
+        title: "React",
+        icon: <Reacticon />,
+    },
+    {
+        title: "Chrome",
+        icon: <ChromeIcon />,
+    },
+    {
+        title: "Github",
+        icon: <GithubIcon />,
+    },
+];
--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,14 +1,45 @@
+import { Card } from "@/components/Card";
 import { SectionHeader } from "@/components/SectionHeader";
-
+import StarIcon from "@/assets/icons/star.svg"
+import bookImage from "@/assets/images/book-cover.png"
+import Image from "next/image";
+import { toolboxItems } from "@/components/TechStack";
 
 export const AboutSection = () => {
   return(
-    <div>
+    <div className="pb-96">
       <SectionHeader 
       eyebrow="About Me" 
       title="A Glimpse Into My World" 
       description="Learn more about I am, what I do, and what Inspires Me."
       />
+      <div>
+        <Card>
+          <div>
+            <StarIcon />
+            <h3>My Reads</h3>
+            <p>Explore the books shaping my perspections.</p>
+          </div>
+          <Image src={bookImage} alt="Book cover" />
+        </Card>
+
+        <Card>
+          <div>
+            <StarIcon />
+            <h3>My Toolbox</h3>
+            <p>Explore the Technologies and Tools use to craft Exceptional Digital Experiences.</p>
+          </div>
+          <div>
+            {toolboxItems.map(item =>(
+              <div key={item.title}>
+                <span>{item.icon}</span>
+                <span>{item.title}</span>
+              </div>
+            ))}
+          </div>
+        </Card>
+
+      </div>
     </div>
   )
 };
