3b6deeea273766b5d3eff8f8f69c36a52744f3d2
Ayush H <jqghebri@gmail.com>
2024-10-03 18:13:05 +0530

    creation

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -5,6 +5,9 @@ export const ContactSection = () => {
   return (
     <div>
       <h2>Let's Create Something Amazing Together</h2>
+      <p>
+        Passionate About Turning Visions into Reality? Let’s Connect and Discuss How I Can Help You Achieve Your Goals.
+      </p>
     </div>
   )
 };
