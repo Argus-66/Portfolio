0e0e55e68ad7cd793d1fdb3d1c7d15bdebf05713
Ayush H <jqghebri@gmail.com>
2024-10-05 17:28:40 +0530

    creation

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,40 @@
+import ArrowUpRighticon from "@/assets/icons/arrow-up-right.svg"
+
+const footerLinks = [
+  {
+    title: "Linkedin",
+    href: "#",
+  },
+  {
+    title: "Github",
+    href: "#",
+  },
+  {
+    title: "Twitter",
+    href: "#",
+  },
+  {
+    title: "Instagram",
+    href: "#",
+  },
+]
+
 export const Footer = () => {
-  return <div>Footer Section</div>;
+  return (
+    <footer>
+      <div className="max-w-[90%] md:max-w-4xl lg:max-w-[90rem] mx-auto">
+        <div>
+          <div>&copy; 2024. All rights reserved.</div>
+          <nav>
+            {footerLinks.map(link =>(
+              <a href="" key={link.title}>
+                <span>{link.title}</span>
+                <ArrowUpRighticon className="" />
+              </a>
+            ))}
+          </nav>
+        </div>
+      </div>
+    </footer>
+  )
 };
