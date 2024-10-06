d83c6f869ced44e4fa03b1215ba1a233c7d72cc7
Ayush H <jqghebri@gmail.com>
2024-10-06 19:21:22 +0530

    creation

--- src/components/StarsCanvas.tsx
+++ src/components/StarsCanvas.tsx
@@ -1,18 +1,14 @@
 "use client";
 
-import React, { useState, useRef, Suspense } from "react";
+import React, { useState, useRef, Suspense, useMemo } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial } from "@react-three/drei";
 // @ts-ignore
 import * as random from "maath/random/dist/maath-random.esm";
-import dynamic from 'next/dynamic'
-
 
 const StarBackground = (props: any) => {
     const ref = useRef<any>();
-    const [sphere] = useState(() =>
-        random.inSphere(new Float32Array(5000), { radius: 1.2 })
-    );
+    const sphere = useMemo(() => random.inSphere(new Float32Array(5000), { radius: 1.2 }), []);
 
     useFrame((state, delta) => {
         ref.current.rotation.x -= delta / 10;
@@ -25,7 +21,7 @@ const StarBackground = (props: any) => {
                 <PointMaterial
                     transparent
                     color="#fff"
-                    size={0.002}
+                    size={0.001}  // Adjusted size for performance
                     sizeAttenuation
                     depthWrite={false}
                 />
@@ -37,7 +33,7 @@ const StarBackground = (props: any) => {
 const StarsCanvas = () => (
     <div className="w-full h-auto fixed inset-0 z-[0]">
         <Canvas camera={{ position: [0, 0, 1] }}>
-            <Suspense fallback={null}>
+            <Suspense fallback={<div>Loading...</div>}>  {/* Optional loading fallback */}
                 <StarBackground />
             </Suspense>
         </Canvas>
